7f01129754e0f6fe68252388fedafac9249e551e
Robert Peltekov <rpeltekov@gmail.com>
2022-12-20 02:30:20 -0800

    printing debug info

--- lora/robot.py
+++ lora/robot.py
@@ -268,6 +268,7 @@ class Robot:
         for step in range(8):
             print("doing incr")
             while not self.check_recieved_ping():
+                print(self.checked_rec_cnt, self.ping_rec_cnt)
                 time.sleep(.1)
             rssi_vals.append(self.radio.last_rssi)
             print(f"got rssi {self.radio.last_rssi}")
