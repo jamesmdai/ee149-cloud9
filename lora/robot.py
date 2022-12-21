9a573cdd8e82eb0e8dff018cfd67a02a6cfdd86c
Robert Peltekov <rpeltekov@gmail.com>
2022-12-21 08:19:51 -0800

    max duty 35

--- lora/robot.py
+++ lora/robot.py
@@ -279,7 +279,7 @@ class Robot:
                 max_step = step
                 max_seen = self.radio.last_rssi
             time.sleep(1.5)
-            self.motor_encoder_move(rotations=2.25)
+            self.motor_encoder_move(rotations=2.25, maxduty=35)
         time.sleep(1)
         print(f"starting move to max_step # {max_step}")
         # for step in range(max_step):
