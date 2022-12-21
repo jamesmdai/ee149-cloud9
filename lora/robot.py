1e454f9bfb06f930d22367cf837949d600a6830a
James Dai <jamesmdai@gmail.com>
2022-12-20 22:15:43 -0800

    anotha attempt

--- lora/robot.py
+++ lora/robot.py
@@ -114,8 +114,7 @@ class Robot:
     def refresh_display(self):
         self.display.fill(0)
         self.display.text(
-                f"G: {self.gear.value} T: {self.turn.value}" +
-                "D: T" if self.discover_mode else "D: F" +
+                f"G: {self.gear.value} T: {self.turn.value} D: {self.discover_mode}"+
                 f"\nPKTS_RCVD: {self.num_packets}" +
                 f"\nTEM: {self.temperature} HUM: {self.humidity}" + 
                 f"\nRSSI: {self.last_rssi}",
